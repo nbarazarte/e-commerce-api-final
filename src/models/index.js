0d1abe0418a468252cbdf8387284bb1d859b7434
Neel <ezebarazarte@gmail.com>
2023-07-04 14:20:26 -0400

    proyecto final de nodejs

---
+++ src/models/index.js
@@ -0,0 +1,30 @@
+const Product = require("./Product")
+const Category = require("./Category")
+const Cart = require("./Cart")
+const User = require("./User")
+const Purchase = require("./Purchase")
+const ProductImg = require("./ProductImg")
+
+//Product -> //categoryId
+Product.belongsTo(Category)
+Category.hasMany(Product)
+
+//Cart -> //userId
+Cart.belongsTo(User)
+User.hasOne(Cart)
+
+//Cart -> //productId
+Cart.belongsTo(Product)
+Product.hasMany(Cart)
+
+//Purchase -> //userId
+Purchase.belongsTo(User)
+User.hasMany(Purchase)
+
+//Purchase -> //productId
+Purchase.belongsTo(Product)
+Product.hasMany(Purchase)
+
+//ProductImg -> //productid
+ProductImg.belongsTo(Product)
+Product.hasMany(ProductImg)
